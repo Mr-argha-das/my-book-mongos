bb4a93e334f2d6030ebd704190c943fb8f5f5ac1
arghaahit <argha.ahit@gmail.com>
2023-08-26 12:22:42 +0530

    login changes

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,39 @@
 const user = require("../models/user")
 
 const userCreate = async(req,res)=>{
-    const createData = await user.create(req.body)
-    res.send(createData)
+    
+    const checkmail = await user.findOne({email: req.body.email})
+    const checknumber = await user.findOne({number: req.body.number})
+    console.log(checkmail)
+    if(checkmail != null || checknumber != null){
+        return res.send({
+           status: false,
+           msg: "user already register"
+        })
+    } else {
+        const createData = await user.create(req.body)
+        res.send({ 
+        status: true,
+        data: createData,
+        msg: "User Create done"
+    })
+    }
+    
 }
 const loginUser = async(req,res)=>{
-    const checkUser = await user.findOne({email:req.body.email,password:req.body.password})
-    if(!checkUser) return res.status(401).send({status:false,msg:'email or password is wrong',data:null})
+     checkUser = await user.findOne({email:req.body.numoremail,password:req.body.password})
+
+    if(!checkUser){
+        checkUser = await user.findOne({number:req.body.numoremail,password:req.body.password})
+        if(!checkUser){
+            return res.status(401).send({
+                status:false,msg:'email or password is wrong',data:null})
+        }
+    }
+    
     res.send({status:true,msg:'login',data:checkUser})
 }
 module.exports = {
     userCreate:userCreate,
     userLogin:loginUser
-}
+}   
--- models/user.js
+++ models/user.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
     name:{type:String,required:true},
     number:{type:String,required:true},
     email:{type:String,required:true},
-    password:{type:String,required:true}
+    password:{type:String,required:true},
+    gender: {type: String, required: true},
+    dob: {type: String, required: true}
 })
 module.exports = mongoose.model('user',userSchema)
